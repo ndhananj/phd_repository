4fce6a2c2c455008d8d44424c15e6d7811f93cb0
MillerMC96 <pywang@ucdavis.edu>
2020-09-27 17:39:37 -0700

    input and output

--- get_short_axis_involvement.py
+++ get_short_axis_involvement.py
@@ -1,6 +1,19 @@
 # Script to find the short axis distance in a certain mode
 import numpy as np
 import matplotlib.pyplot as plt
-import os
+import sys
 from biopandas.pdb import PandasPdb
 from modes import shift_by_mode
+
+if __name__ == "__main__":
+    # input pdb
+    start_pdb = sys.argv[1]
+
+    # eigenmatrix
+    eigenmatrix = sys.argv[2]
+
+    # output figure filename
+    if sys.argv[3]:
+        output_graph = sys.argv[3]
+    else:
+        output_graph = "short_axis_distance_across_all_modes"
