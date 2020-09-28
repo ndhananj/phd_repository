f945ccc2e652acadd79d40cbd66f66c80e9f445c
MillerMC96 <pywang@ucdavis.edu>
2020-09-27 17:06:59 -0700

    init

---
+++ get_short_axis_involvement.py
@@ -0,0 +1,6 @@
+# Script to find the short axis distance in a certain mode
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from biopandas.pdb import PandasPdb
+from modes import shift_by_mode
