6233a19a9656e500a2409781f5af3696cff934f9
MillerMC96 <pywang@ucdavis.edu>
2020-09-29 16:15:37 -0700

    short axis atom numbers

--- get_short_axis_involvement.py
+++ get_short_axis_involvement.py
@@ -10,9 +10,19 @@ from modes import shift_by_mode
 # store the short axis distance
 # plot the short axis distance against modes to figure out peaks
 
+def get_short_axis_distance():
+    pass
+
+def plot_short_axis_spectrum():
+    pass
+
 if __name__ == "__main__":
     # input pdb
     start_pdb = sys.argv[1]
+
+    # read in as BioPandas Data Frame
+    ppdb = PandasPdb()
+    ppdb.read_pdb(start_pdb)
 
     # eigenmatrix
     eigenmatrix = sys.argv[2]
@@ -22,3 +32,7 @@ if __name__ == "__main__":
         output_graph = sys.argv[3]
     else:
         output_graph = "short_axis_distance_across_all_modes"
+
+    # atom numbers of short axis
+    ALA63 = 491
+    PHE28 = 212
