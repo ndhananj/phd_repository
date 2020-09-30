6420e474b07f0c6511f5c2309762adbf616dbd22
MillerMC96 <pywang@ucdavis.edu>
2020-09-29 17:23:18 -0700

    finished short axis distance function

--- get_short_axis_involvement.py
+++ get_short_axis_involvement.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 from biopandas.pdb import PandasPdb
-from modes import shift_by_mode
+from modes import shift_by_mode, match_col_in_int_list
 
 # shift the original pdb by a fixed amplitude
 # calculate the short axis distance of this mode
@@ -12,7 +12,10 @@ from modes import shift_by_mode
 
 # get the short axis distance
 def get_short_axis_distance(atom1, atom2):
-    pass
+    x2 = np.square(atom1[0] - atom2[0])
+    y2 = np.square(atom1[1] - atom2[1])
+    z2 = np.square(atom1[2] - atom2[2])
+    return np.sqrt(x2 + y2 + z2)
 
 # plotting function
 def plot_short_axis_spectrum():
@@ -20,7 +23,9 @@ def plot_short_axis_spectrum():
 
 # find atom coordinates based on atom number
 def get_atom_coord(df, atom_number):
-    coord = df['ATOM'][df['ATOM']['atom_number'] == atom_number]
+    stat_items = ['x_coord', 'y_coord', 'z_coord']
+    coord = df['ATOM'][df['ATOM']['atom_number'] == atom_number] \
+            [stat_items].to_numpy()[0]
     return coord
 
 if __name__ == "__main__":
