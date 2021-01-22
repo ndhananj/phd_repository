e9d84e88d76a852bf1ef7441a12e44c499358177
Nithin Dhananjayan <nithin.dhananjayan@gmail.com>
2021-01-22 14:56:12 -0800

    Change plotting of transformed coords

--- plot_transformed_coords.py
+++ plot_transformed_coords.py
@@ -4,7 +4,7 @@ from modes import *
 
 def plot_transformed_coords(\
     coordinates='transformed_coords.npy',\
-    start_mode=0, end_mode=9):
+    start_mode=0, end_mode=9,time_start=1500):
     coords = load_matrix(coordinates)
     for i in range(start_mode,end_mode+1):
         plot_transformed(coords[:,i],i)
@@ -16,4 +16,6 @@ if __name__ == '__main__':
        else 0
     end_mode = sys.argv[3] if len(sys.argv)>3 \
        else 9
-    plot_transformed_coords(coordinates,start_mode,end_mode)
+    time_start = float(sys.argv[4]) if len(sys.argv)>3 \
+        else 1500
+    plot_transformed_coords(coordinates,start_mode,end_mode,time_start)
