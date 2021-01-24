8475363aae819bd9cfdf12b44625b0d08cafb50b
Nithin Dhananjayan <nithin.dhananjayan@gmail.com>
2021-01-24 04:55:12 -0800

    Add conversion to boolean for outputforchunks

--- get_covariance_matrix_and_stats.py
+++ get_covariance_matrix_and_stats.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     xvg_input = sys.argv[1]
     pdbToAlign = sys.argv[2] if len(sys.argv)>2 else None
     #parameters
-    outputForChunks = sys.argv[3] if len(sys.argv)>3 else False
+    outputForChunks = bool(sys.argv[3]) if len(sys.argv)>3 else False
     unbias = sys.argv[4] if len(sys.argv)>4 else False
     #outputs
     coordinates = sys.argv[5] if len(sys.argv)>5 \
